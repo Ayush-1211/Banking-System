eef07acc9ba039260d31ac32752a2cd89e7d3dff
Ayush-1211 <63777803+Ayush-1211@users.noreply.github.com>
2021-01-10 13:48:56 +0530

    Update Indian_bank.sql

--- sql/Indian_bank.sql
+++ sql/Indian_bank.sql
@@ -1,25 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 19, 2020 at 10:01 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `sparks_bank`
---
 
 -- --------------------------------------------------------
 
@@ -97,6 +81,4 @@ ALTER TABLE `users`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
